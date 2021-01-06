8fb74bb42604d9292368d368c4efb77125125df2
Arie Peretz <ariep20@gmail.com>
2021-01-06 12:07:29 +0200

    initial commit

---
+++ ExampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleProject
+//
+//  Created by Arie Peretz on 06/01/2021.
+//  Copyright © 2021 Arie Peretz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
